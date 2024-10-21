debc3f24a6605a37bd3abe1222698c45fc549296
webQbe <webcubesolutionslk@gmail.com>
2024-10-21 14:43:39 +0530

    Logged updated waiting time to the launch date

--- js/main.js
+++ js/main.js
@@ -2,12 +2,17 @@
 // Select div.countdown
 const countdown = document.querySelector('.countdown');
 
-// Set Launch Date
+// Set Launch Date in milliseconds
 const launchDate = new Date('Jan 1, 2025 13:00:00').getTime();
 
 // Update every second
 const update = setInterval(() => {
 
-    console.log('Update');
+    // Get current date & time in milliseconds
+    const now = new Date().getTime();    
+
+    // Calculate waiting time to the launch date
+    const waitTime = launchDate - now;
+    console.log(waitTime);
 
 }, 1000); // Run Every 1 sec
