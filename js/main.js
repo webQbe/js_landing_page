0022ef0f783286c3bea074789b929bc39ffdb881
webQbe <webcubesolutionslk@gmail.com>
2024-10-21 14:37:31 +0530

    Created function to run every 1 sec

--- js/main.js
+++ js/main.js
@@ -4,4 +4,10 @@ const countdown = document.querySelector('.countdown');
 
 // Set Launch Date
 const launchDate = new Date('Jan 1, 2025 13:00:00').getTime();
-console.log(launchDate);
+
+// Update every second
+const update = setInterval(() => {
+
+    console.log('Update');
+
+}, 1000); // Run Every 1 sec
