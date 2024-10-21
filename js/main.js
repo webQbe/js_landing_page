9edabe20f0bcbe3eed1f55b0180f091b337fe518
webQbe <webcubesolutionslk@gmail.com>
2024-10-21 14:32:07 +0530

    Logged launch date in milliseconds

---
+++ js/main.js
@@ -0,0 +1,7 @@
+
+// Select div.countdown
+const countdown = document.querySelector('.countdown');
+
+// Set Launch Date
+const launchDate = new Date('Jan 1, 2025 13:00:00').getTime();
+console.log(launchDate);
